40908d4b6f340adf91878e5afc0a3c4fdf4e0b2b
Mohammad hmedat <humedat23@gmail.com>
2025-02-06 23:59:48 +0200

    some changes on favourite page

--- lib/pages/favouritePage.dart
+++ lib/pages/favouritePage.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:fizzflow/models/items.dart';
+import 'package:fizzflow/widgets/favourite_item.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -8,59 +9,26 @@ class favouritePage extends StatefulWidget {
   const favouritePage({super.key});
 
   @override
-  State<favouritePage> createState() => _favouritePageState();
+  State<favouritePage> createState() => favouritePageState();
 }
 
-class _favouritePageState extends State<favouritePage> {
+class favouritePageState extends State<favouritePage> {
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Card(
-          elevation: 5,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Padding(
+    return SingleChildScrollView(
+      child: Column(children: [
+        Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Image.network(
-                  item[11].imageurl,
-                  height: 70,
-                  fit: BoxFit.cover,
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '${item[11].name} \$',
-                        style: GoogleFonts.playfair(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black54,
-                        ),
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        '${item[11].price} \$',
-                        style: GoogleFonts.poppins(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.deepOrange),
-                      ),
-                    ],
-                  ),
-                ),
-                Icon(Icons.favorite, color: Colors.red)
-              ],
-            ),
-          ),
-        ),
-      ),
-    ]);
+            child: ListView.builder(
+              itemCount: item.length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: FavouriteItem(item: item[index]),
+                );
+              },
+            )),
+      ]),
+    );
   }
 }
---
+++ lib/widgets/favourite_item.dart
@@ -0,0 +1,54 @@
+import 'package:fizzflow/models/items.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class FavouriteItem extends StatelessWidget {
+  final Items item;
+  FavouriteItem({super.key, required this.item});
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Image.network(
+              item.imageurl,
+              height: 70,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${item.name} \$',
+                    style: GoogleFonts.playfair(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black54,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    '${item.price} \$',
+                    style: GoogleFonts.poppins(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.deepOrange),
+                  ),
+                ],
+              ),
+            ),
+            Icon(Icons.favorite, color: Colors.red)
+          ],
+        ),
+      ),
+    );
+  }
+}
